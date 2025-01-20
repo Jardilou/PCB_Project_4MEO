ba008073d9d0187bf606197282dd70a220fd2848
Jardilou <78799495+Jardilou@users.noreply.github.com>
2025-01-20 12:14:54 +0100

    Update FSM_PCB_Project_Without_Coms.ino

--- FSM_PCB_Project_Without_Coms.ino
+++ FSM_PCB_Project_Without_Coms.ino
@@ -14,16 +14,17 @@ enum State {
 State currentState = IDLE;
 // Pin definitions
 const int actuatorPin = 48; // Passive buzzer + fan pin
-const int MQ135Pin = 13;    // MQ135 digital pin previously calibrated via the potentiometer
-const int MiCS6814Pin_CO = 37;  // MiCS6814 analog pin
-const int MiCS6814Pin_NO2 = 38; // MiCS6814 analog pin
-const int MiCS6814Pin_NH3 = 45; // MiCS6814 analog pin
+const int MQ135Pin = A0;    // MQ135 digital pin previously calibrated via the potentiometer
+const int MiCS6814Pin_CO = A3;  // MiCS6814 analog pin
+const int MiCS6814Pin_NO2 = A1; // MiCS6814 analog pin
+const int MiCS6814Pin_NH3 = A2; // MiCS6814 analog pin
 
 
 // Thresholds
-const int MICS6814_CO_THRESHOLD = 3.5;     // Threshold for MiCS6814 CO in ppm : values retreived from WHO
-const int MICS6814_NO2_THRESHOLD = 0.015;    // Threshold for MiCS6814 NO2 in ppm : values retreived from WHO
-const int MICS6814_NH3_THRESHOLD = 0.57;    // Threshold for MiCS6814 NH3 : values retreived from oizom, a NH3 monitoring sensor manufacturer
+const float MICS6814_CO_THRESHOLD = 5;     // Threshold for MiCS6814 CO in ppm : values retreived from WHO : 3.5
+const float MICS6814_NO2_THRESHOLD = 0.2;    // Threshold for MiCS6814 NO2 in ppm : values retreived from WHO : 0.015
+const float MICS6814_NH3_THRESHOLD = 0.8;    // Threshold for MiCS6814 NH3 : values retreived from oizom,
+// a NH3 monitoring sensor manufacturer : 0,57
 //For the previously calibrated CO2 sensor MQ135 we put a threshold for digital state of 1000 ppm 
 //(values retrieved from haut conseil de la santé publique)
 
@@ -35,7 +36,9 @@ MICS6814 gas(MiCS6814Pin_CO, MiCS6814Pin_NO2, MiCS6814Pin_NH3);
 
 void setup() {
   // Initialize serial communication
-  Serial.begin(115200);
+  Serial.begin(9600);
+  //Calibrate the MiCS6814
+  gas.calibrate();
   // Initialize pins
   pinMode(actuatorPin, OUTPUT);
   pinMode(MQ135Pin, INPUT);
@@ -78,21 +81,28 @@ void detectingState() {
 
     // Read sensors
     int mq135Value = digitalRead(MQ135Pin); // Once the sensor is calibrated
-    //int mq135Value =  mq135_sensor.getPPM(); //To calibrate for co2 values
+    //int mq135Value =  mq135_sensor.getPPM(); //To calibrate for co2 values. Careful, it may take a long time
 
-    int mics6814Value_CO = gas.measure(CO);
-    int mics6814Value_NO2 = gas.measure(NO2);
-    int mics6814Value_NH3 = gas.measure(NH3);
+    float mics6814Value_CO = gas.measure(CO);
+    float mics6814Value_NO2 = gas.measure(NO2);
+    float mics6814Value_NH3 = gas.measure(NH3);
 
     // Log sensor readings
     Serial.print("MQ135 Value: ");
     Serial.println(mq135Value);
+    Serial.print(" ppm");
+
     Serial.print("MiCS6814 CO: ");
     Serial.println(mics6814Value_CO);
+    Serial.print(" ppm");
+
     Serial.print("MiCS6814 NO2: ");
     Serial.println(mics6814Value_NO2);
+    Serial.print(" ppm");
+
     Serial.print("MiCS6814 NH3: ");
     Serial.println(mics6814Value_NH3);
+    Serial.print(" ppm");
 
     // Check thresholds
     if (mq135Value > 0 || mics6814Value_CO > MICS6814_CO_THRESHOLD ||
@@ -108,5 +118,5 @@ void detectingState() {
 void alarmState() {
   digitalWrite(actuatorPin, HIGH); // Activate buzzer and fan
   Serial.println("ALARM: Hazardous gas levels detected!");
-  
+  delay(1000);
 }
